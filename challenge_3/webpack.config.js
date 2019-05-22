ae79ac48a3102731da5b819ccaf2976d103c65d1
Michael Ryan <michaeltheryan@gmail.com>
2019-05-22 07:52:25 -0400

    Init and install of dependencies

---
+++ challenge_3/webpack.config.js
@@ -0,0 +1,35 @@
+﻿const path = require("path"); // if using path.resolve(__dirname, ‘[file]’)
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: "/assets/"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader"
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  devServer: {
+    publicPath: "http://localhost:8080/dist/",
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebPackPlugin()
+  ]
+};

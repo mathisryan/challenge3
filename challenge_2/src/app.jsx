a849eb4ba7a34ba8c69ffe7ff81ec94f6fdf9875
Michael Ryan <michaeltheryan@gmail.com>
2019-05-22 00:08:21 -0400

    Initial install of dependencies

---
+++ challenge_2/src/app.jsx
@@ -0,0 +1,7 @@
+import { hot } from 'react-hot-loader/root';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => <div>???</div>;
+export default hot(App);
+
---
+++ challenge_2/webpack.config.js
@@ -0,0 +1,36 @@
+﻿const path = require("path"); // if using path.resolve(__dirname, ‘[file]’)
+const webpack = require("webpack");  // if using plugins
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/assets/'
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader"
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  devServer: {
+    publicPath: "http://localhost:8080/dist/",
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebPackPlugin()
+  ]
+};

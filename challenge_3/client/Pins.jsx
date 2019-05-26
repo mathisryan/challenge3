b9e56fb1e0705870533df056e0235b9623dc31b7
Michael Ryan <michaeltheryan@gmail.com>
2019-05-26 17:41:49 -0400

    Added a Pins component to display pin numbers in a table
    Refactored App to render Pins correctly
    
    Refactored Pins to make pins displayed
    dependent on number of pins stored in state

---
+++ challenge_3/client/Pins.jsx
@@ -0,0 +1,87 @@
+﻿// import { hot } from 'react-hot-loader'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Pins(props) {
+  function handleSelect(count) {
+    props.handleChoice(count)
+  }
+
+  return (
+    <div>
+      <table>
+        <tbody>
+          {props.upright > 0 && <FirstRow upright={props.upright} handleClick={handleSelect} />}
+        </tbody>
+        <tbody>
+          {props.upright > 1 && <SecondRow upright={props.upright} handleClick={handleSelect} />}
+        </tbody>
+        <tbody>
+          {props.upright > 3 && <ThirdRow upright={props.upright} handleClick={handleSelect} />}
+        </tbody>
+        <tbody>
+          {props.upright > 6 && <FourthRow upright={props.upright} handleClick={handleSelect} />}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+function FirstRow(props) {
+  return(
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td onClick={() => props.handleClick(1)}>One</td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  )
+}
+
+function SecondRow(props) {
+  return(
+    <tr>
+      <td></td>
+      <td></td>
+      <td onClick={() => props.handleClick(2)}>Two</td>
+      <td></td>
+      {props.upright > 2 ? <td onClick={() => props.handleClick(3)}>Three</td> : <td></td>}
+      <td></td>
+      <td></td>
+    </tr>
+  )
+}
+
+function ThirdRow(props) {
+  return(
+    <tr>
+      <td></td>
+      <td onClick={() => props.handleClick(4)}>Four</td>
+      <td></td>
+      {props.upright > 4 ? <td onClick={() => props.handleClick(5)}>Five</td> : <td></td>}
+      <td onClick={() => props.handleClick(5)}>Five</td>
+      <td></td>
+      {props.upright > 5 ? <td onClick={() => props.handleClick(6)}>Six</td> : <td></td>}
+      <td></td>
+    </tr>
+  )
+}
+
+function FourthRow(props) {
+  return(
+    <tr>
+      <td onClick={() => props.handleClick(7)}>Seven</td>
+      <td></td>
+      {props.upright > 6 ? <td onClick={() => props.handleClick(8)}>Eight</td> : <td></td>}
+      <td></td>
+      {props.upright > 7 ? <td onClick={() => props.handleClick(9)}>Nine</td> : <td></td>}
+      <td></td>
+      {props.upright > 8 ? <td onClick={() => props.handleClick(10)}>Ten</td> : <td></td>}
+    </tr>
+  )
+}
+
+export default Pins;
--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,18 +1,26 @@
 // import { hot } from 'react-hot-loader'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Pins from './Pins'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {pins: []}
+    this.state = {
+      pins: 10
+    };
+    this.handleInput = this.handleInput.bind(this)
+  }
+
+  handleInput(count) {
+    this.setState({
+      pins: count
+    })
   }
 
   render() {
     return (
-      <div>
-        <p>Test</p>
-      </div>
+      <Pins handleChoice={this.handleInput} upright={this.state.pins}/>
     );
   }
 }
